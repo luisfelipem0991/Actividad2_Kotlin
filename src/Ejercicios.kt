3b208df78f9d01bf2e57f2bc1dd8f6a3a35daadd
luisfelipem0991 <ffelipemilina2008@gmail.com>
2026-05-03 21:22:24 -0500

    se hizo el reto 8

--- src/Ejercicios.kt
+++ src/Ejercicios.kt
@@ -95,3 +95,14 @@ fun reto7() {
     consumir("Arroz", 2)
     println("Final: $despensa")
 }
+// 8. Cálculo de Promedio Académico
+fun reto8() {
+    val notas = listOf(4.5 to 0.2, 3.8 to 0.3, 4.0 to 0.5)
+    var notaFinal = 0.0
+    for (par in notas) {
+        notaFinal += par.first * par.second
+    }
+
+    println("\n--- Reto 8: Promedio Académico ---")
+    println("Nota Final: $notaFinal")
+}
